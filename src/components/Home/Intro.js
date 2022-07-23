795d898a597b107672a595a123dde7f414688ed7
mark0613 <goldffd700@gmail.com>
2022-07-23 16:42:14 +0800

    change: Display the intro by circle.

--- src/components/Home/Intro.js
+++ src/components/Home/Intro.js
@@ -1,79 +1,106 @@
 import React from 'react';
 
-import { Collapse } from 'antd';
-
 import {
-    Typography,
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
+    Box,
+    Grid,
 } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const accordions = [
+const intro = [
     {
-        key : "why",
-        summary : "開發目的",
-        detail : "鑒於學校資訊分散，因此我們開發這個網站用於彙整逢甲大學相關資訊。",
+        key : 'why',
+        title : '開發目的',
+        content : '鑒於學校資訊分散，因此我們開發這個網站用於彙整逢甲大學相關資訊。',
+        color: '#E9F0F7'
     },
     {
-        key : "what",
-        summary : "網站內容",
-        detail : "為了達到資訊傳達的正確性，我們僅僅將資料來源的文章「標題、連結、發布時間」抓取下來並整理成表格，點擊標題即可前往來源文章。",
+        key : 'what',
+        title : '網站內容',
+        content : '為資訊傳達的正確性，僅將資料來源的文章「標題、連結、發布時間」抓取並整理，點擊標題即可前往來源文章。',
+        color: '#EFF6E9'
     },
     {
-        key : "where",
-        summary : "資訊來源",
-        detail : "來自MyFCU、Facebook、Instagram。",
-    },
-    {
-        key : "how",
-        summary : "相關技術",
-        detail : "使用爬蟲撈取個網頁資料後存到資料庫，再以本網頁顯示資料庫內容。",
-    },
-    {
-        key : "who",
-        summary : "聯絡我們",
-        detail : (
+        key : 'where',
+        title : '資訊來源',
+        content : (
             <>
-                <p>Email: project1618033988@gmail.com</p>
-                <p>Github: </p>
+                <p>來自MyFCU、Facebook、Instagram。</p>
+                <p>詳細可參考: 詳細來源</p>
             </>
         ),
+        color: '#FFDBEB'
     },
-]
+    {
+        key : 'how',
+        title : '相關技術',
+        content : '使用爬蟲撈取個網頁資料後存到資料庫，再以本網頁顯示資料庫內容。',
+        color: '#FFF7E0'
+    },
+];
 
-const { Panel } = Collapse;
-
-export function Intro(props) {
-    const result = [];
-    for (let acc of accordions) {
-        result.push(
-            // <Accordion expanded={props.expanded === acc.key} onChange={props.handleChange(acc.key)} sx={{ textAlign: 'left' }}>
-            //     <AccordionSummary expandIcon={<ExpandMoreIcon />} >
-            //         <Typography variant='h6' sx={{ width: '33%', flexShrink: 0 }}>
-            //             {acc.summary}
-            //         </Typography>
-            //     </AccordionSummary>
-            //     <AccordionDetails>
-            //         <Typography>
-            //             {acc.detail}
-            //         </Typography>
-            //     </AccordionDetails>
-            // </Accordion>
-            
-            <Panel header={acc.summary} key={acc.key}>
-                <p>{acc.detail}</p>
-            </Panel>
-        )
-        
-    }
-    let text = "Hello"
+export function Intro() {
     return (
-        <Collapse accordion>
-            {result}
-        </Collapse>
-
-    );
-
+        <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+            sx={{ height: 600 }}
+        >
+            <Grid item xs={0} lg={1} />
+            <Grid item xs={12} lg={10}>
+                <Box
+                    sx={{
+                        fontSize: '38px',
+                        color: '#3C51B6',
+                        fontWeight: 'bold',
+                        marginBottom: "20px",
+                    }}
+                >
+                    網站簡介
+                </Box>
+                <Grid
+                    container
+                    direction='row'
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    {intro.map(item => (
+                    <Grid 
+                        item 
+                        xs={12} md={6} lg={3}
+                    >
+                        <Box
+                            bgcolor={item.color}
+                            sx={{
+                                height: 300,
+                                width: 300,
+                                padding: '40px',
+                                borderRadius: '100%'
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    fontSize: '26px',
+                                    color: '#3C51B6',
+                                    fontWeight: 'bold',
+                                    marginBottom: "20px",
+                                }}
+                            >
+                                {item.title}
+                            </Box>
+                            <Box
+                                sx={{
+                                    fontSize: '18px'
+                                }}
+                            >
+                                {item.content}
+                            </Box>
+                        </Box>
+                    </Grid>
+                ))}
+                </Grid>
+            </Grid>
+            <Grid item xs={0} lg={1} />
+        </Grid>
+    )
 }
