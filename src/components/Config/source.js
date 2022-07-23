b827ba9e6c82956302dc41c1787b6d2461ed8100
mark0613 <goldffd700@gmail.com>
2022-07-23 14:28:38 +0800

    change: Cnange all detail of sources.

--- src/components/Config/source.js
+++ src/components/Config/source.js
@@ -1,49 +1,100 @@
-function wrapLinks(json) {
-    let result = [];
-    for (let key in json) {
-        result.push(
-            <a href={json[key]} target="_blank">{key}</a>
-        );
-    }
-}
+const myfcu = [
+    {
+        title: "MyFCU",
+        key: "MyFCU",
+        link: "https://myfcu.fcu.edu.tw/main/infomyfculogin.aspx",
+        class: "一般",
+    },
+]
 
-const myfcu = {
-    MyFcu: "https://myfcu.fcu.edu.tw/main/infomyfculogin.aspx",
-}
-const fb = {
-    GENEDU: "https://www.facebook.com/GENEDU.FCU/",
-    iSchool: "https://www.facebook.com/iSchool.FCU/",
-    dSchool: "https://www.facebook.com/fcudschool/",
-    CheckInAtFcu: "https://www.facebook.com/checkinatfcu/",
-    SaInFCU: "https://www.facebook.com/SAinFCU/",
-}
-const ig = {
-    Sa: "https://www.instagram.com/fcu_sa/",
-    Coiee: "https://www.instagram.com/fcucoiee/",
-    FengChiaUniversity: "https://www.instagram.com/fengchia_university/",
-    CheckInAtFcu: "https://www.instagram.com/checkinatfcu/",
-    AfterClass: "https://www.instagram.com/fcu_afterclass/",
-    IecsClass: "https://www.instagram.com/iecs.class/",
-}
+const fb = [
+    {
+        title: "通識中心",
+        key: "GENEDU",
+        link: "https://www.facebook.com/GENEDU.FCU/",
+        class: "一般",
+    },
+    {
+        title: "創能學院",
+        key: "iSchool",
+        link: "https://www.facebook.com/iSchool.FCU/",
+        class: "一般",
+    },
+    {
+        title: "跨領域設計學院",
+        key: "dSchool",
+        link: "https://www.facebook.com/fcudschool/",
+        class: "一般",
+    },
+    {
+        title: "逢甲學記",
+        key: "CheckInAtFcu",
+        link: "https://www.facebook.com/checkinatfcu/",
+        class: "一般",
+    },
+    {
+        title: "學生會",
+        key: "SaInFCU",
+        link: "https://www.facebook.com/SAinFCU/",
+        class: "一般",
+    },
+]
+
+const ig = [
+    {
+        title: "學生會",
+        key: "Sa",
+        link: "https://www.instagram.com/fcu_sa/",
+        class: "一般",
+    },
+    {
+        title: "資電學院",
+        key: "Coiee",
+        link: "https://www.instagram.com/fcucoiee/",
+        class: "資電學院",
+    },
+    {
+        title: "逢甲大學",
+        key: "FengChiaUniversity",
+        link: "https://www.instagram.com/fengchia_university/",
+        class: "一般",
+    },
+    {
+        title: "逢甲學記",
+        key: "CheckInAtFcu",
+        link: "https://www.instagram.com/checkinatfcu/",
+        class: "一般",
+    },
+    {
+        title: "下課之後",
+        key: "AfterClass",
+        link: "https://www.instagram.com/fcu_afterclass/",
+        class: "一般",
+    },
+    {
+        title: "資訊系",
+        key: "IecsClass",
+        link: "https://www.instagram.com/iecs.class/",
+        class: "資電學院",
+    },
+]
+
 
 
 export const sourceConfig = [
     {
         title: "MyFCU",
-        detail: Object.keys(myfcu),
-        links: wrapLinks(myfcu),
         key: "myfcu",
+        data: myfcu,
     },
     {
         title: "Facebook",
-        detail: Object.keys(fb),
-        links: wrapLinks(fb),
         key: "fb",
+        data: fb,
     },
     {
         title: "Instagram",
-        detail: Object.keys(ig),
-        links: wrapLinks(ig),
         key: "ig",
+        data: ig,
     },
 ]
