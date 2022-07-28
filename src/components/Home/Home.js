2a6000762b9cda305b6b37ef5f3266c610318a37
mark0613 <goldffd700@gmail.com>
2022-07-29 00:10:01 +0800

    change: Remove Contact

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import 'antd/dist/antd.min.css';
 import { Layout } from 'antd';
 
-import { Box } from '@mui/material';
-
-import { Navbar } from '../Layout';
+import { 
+    Navbar,
+    Foot,
+} from '../Layout';
 import { Topic } from './Topic';
 import { Intro } from './Intro';
 import { Source } from './Source';
-import { Contact } from './Contact';
 
 
 const { Content } = Layout;
@@ -16,16 +16,14 @@ export function Home() {
     return (
         <Layout>
             <Navbar style={{ position: 'fixed', zIndex: 1, width: '100%' }} />
-
             <Content className="site-layout" style={{ marginTop: 64 }}>
                 <div className="site-layout-background" style={{ padding: "0 24px", minHeight: 600, textAlign: "center" }}>
                     <Topic />
                     <Intro />
                     <Source />
-                    <Contact />
                 </div>
             </Content>
-            <Box sx={{ height: "60px", bgcolor: {xs: "transparent", md: '#FFF'} }} />
+            <Foot />
         </Layout>
     )
 }
