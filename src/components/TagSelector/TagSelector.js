e8ba9155b3461ab9cf3c445ec288219e2b37cd8e
mark0613 <goldffd700@gmail.com>
2022-07-30 23:04:00 +0800

    change: Change display

--- src/components/TagSelector/TagSelector.js
+++ src/components/TagSelector/TagSelector.js
@@ -1,48 +1,21 @@
 import { Select } from 'antd';
-
-import { sourceConfig } from '../Config';
-
-
-function generateOptions(sourceName) {
-    let source = [];
-    for (let config of sourceConfig) {
-        if (config.key === sourceName) {
-            source = config.data;
-            break;
-        }
-    }
-
-    const optionClass = [];
-    const options = [];
-    for (let item of source) {
-        if (!optionClass.includes(item.class)) {
-            optionClass.push(item.class);
-            options.push({
-                label: item.class,
-                options: [],
-            })
-        }
-        let index = optionClass.indexOf(item.class);
-        options[index].options.push({
-            label: item.title,
-            value: item.title,
-            id: `${options[index].label}-${item.title}`,
-        })
-    }
-    return options
-}
+import { DeleteOutlined } from '@ant-design/icons';
 
 
 export function TagSelector(props) {
-    const options = generateOptions(props.sourceName);
-
     return (
         <Select
             mode="multiple"
-            style={{ width: '100%' }}
+            options={props.options}
+            defaultValue={props.defaultValue}
+            onChange={props.onChange}
+            autoClearSearchValue={true}
             placeholder="選擇 Tags"
-            onChange={props.handleChange}
-            options={options}
+            showArrow
+            allowClear
+            maxTagCount={3}
+            style={{ width: '100%', textAlign: 'left' }}
+            clearIcon={<DeleteOutlined style={{color: "red"}} />}
         />
     )
 }
